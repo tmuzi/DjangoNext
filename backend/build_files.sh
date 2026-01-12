04710a8f68ab7c3d6d2fbb111cf30ef4a5d73f93
tmuzi <tafmuzira@gmail.com>
2026-01-12 22:29:24 +0200

    Update build script: add superuser creation step after migrations

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -2,3 +2,4 @@ pip install -r requirements.txt
 python3.12 manage.py collectstatic --no-input --clear
 python3.12 manage.py makemigrations
 python3.12 manage.py migrate
+python3.12 manage.py createsuperuser --noinput
