b8d6ca80fb03da5aff8583b2322eaae87e4c4f6a
tmuzi <tafmuzira@gmail.com>
2026-01-12 02:35:00 +0200

    Uniform cookie lib

--- frontend/app-client/app/middleware.ts
+++ frontend/app-client/app/middleware.ts
@@ -1,10 +1,9 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { cookies } from "next/headers";
+import { getCookie } from 'cookies-next/client';
 
 export function middleware(request: NextRequest) {
-  const cookieStore = cookies();
-  const accessToken = cookieStore.get("accesssToken");
+  const accessToken = getCookie("accesssToken");
 
   if (!accessToken && request.nextUrl.pathname !== "/") {
     return NextResponse.redirect(new URL("/auth/login", request.url));
